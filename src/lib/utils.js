5e61bf1446b8606fc6b9b3eef24b5fd8390735dd
Gran <granvilekaranja@gmail.com>
2024-11-14 17:14:49 +0300

    update fetchUrl

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const fetchUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000/api'
+  : 'https://code-clause-internship-recipe-book-qmpw.vercel.app/api';
